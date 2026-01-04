18fd9a816eb0121a2d71ce3e9149f0273b36eb7c
deepaksy <231075017+tazerface007@users.noreply.github.com>
2026-01-04 23:40:45 +0530

    Add loop.py

---
+++ basics/loops/loops.py
@@ -0,0 +1,10 @@
+# Program to demonstrate the loops in python.
+
+
+def forLoop():
+    for i in range(0,100001):
+        print(f'\t{i}: Hello World!')
+
+
+if __name__ == '__main__':
+    forLoop()
