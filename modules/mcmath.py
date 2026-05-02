6c544278efb5eeecaf9ca1a1f943710e7c54ed5f
tazerface007 <rohandesoza@duck.com>
2026-05-02 06:26:00 +0000

    Add examples for math and cmath modules

---
+++ modules/mcmath.py
@@ -0,0 +1,7 @@
+"""Mathematical function for complex numbers"""
+import cmath
+
+print(cmath.sqrt(-2-0j))
+print(cmath.sqrt(-2+0j))
+print(cmath.phase(3j))
+print(cmath.polar(cmath.phase(3j)))
---
+++ modules/mmath.py
@@ -0,0 +1,15 @@
+import math
+
+print(math.comb(10, 2))
+print(math.factorial(5))
+print(math.gcd(10,2))
+print(math.isqrt(2))
+print(math.lcm(2,4))
+print(math.gcd(2,4)) # Piggy Portion Control It represent the biggest number of people you can invite with having that 2 apple and 4 bananas
+print(math.perm(2,2))
+
+# Floating point arithmetic
+
+print(math.ceil(2.65))
+print(math.floor(2.65))
+print(math.fabs(2.2))
